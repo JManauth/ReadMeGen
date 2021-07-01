b27ee431e1500b97dfc80e87662a51ec97b0630b
jafet <jafet.manauth@hotmail.com>
2021-06-30 22:54:13 -0500

    added functionality of being able to create an md file through node inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your Project Title?', ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Success!')
+    );
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt([
+            {
+                type : 'input',
+                message: questions[0],
+                name: 'Title'
+            }
+        ])
+        .then((response) =>
+            writeToFile('READMETEST.md', response.Title)
+        );
+}
 
 // Function call to initialize app
 init();
